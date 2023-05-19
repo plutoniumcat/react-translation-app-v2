85751eae7f7e392176336b62eb0a2c4df906f65f
radiantbutterfly <alexandrialeemcpherson@gmail.com>
2023-05-20 02:23:30 +0900

    Add state to dropdown menus

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,8 +1,24 @@
 export default function Dropdown(props) {
+    const menuType = props.sourceLang ? "source-lang" : "output-lang";
+
+    const languageSelect = (event) => {
+        if (props.setSourceLang) {
+            props.setSourceLang(event.target.value);
+            console.log(`Source language set to ${event.target.value}`);
+        }
+        else if (props.setOutputLang) {
+            props.setOutputLang(event.target.value);
+            console.log(`Output language set to ${event.target.value}`);
+        }
+        else {
+            console.log("Language dropdown could not detect if it is source or output.");
+        }
+    }
+
   return (
     <div>
-        <label htmlFor="source-lang">Source language:</label>
-        <select name="source-lang">
+        <label htmlFor={ menuType }>Language:</label>
+        <select name={ menuType } onChange={ (e) => languageSelect(e) }>
             <option value="english">English</option>
             <option value="japanese">Japanese</option>
             <option value="french">French</option>
--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,12 +1,17 @@
+import { useState } from "react";
 import Dropdown from "./Dropdown";
 
 export default function Homepage() {
+    const [sourceLang, setSourceLang] = useState("english");
+    const [outputLang, setOutputLang] = useState("english");
+
+
   return (
     <div>
       <form>
-        <Dropdown />
+        <Dropdown sourceLang={ sourceLang } setSourceLang={ setSourceLang } />
         <textarea name="source-text" cols="30" rows="10"></textarea>
-        <Dropdown />
+        <Dropdown outputLang={ outputLang } setOutputLang={ setOutputLang } />
         <textarea name="translated-text" cols="30" rows="10"></textarea>
       </form>
     </div>
