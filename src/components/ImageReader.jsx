ac26222ebd6a15613879937e8adbac708dc7429d
radiantbutterfly <alexandrialeemcpherson@gmail.com>
2023-05-20 03:34:30 +0900

    Add API call to OCR API to ImageReader component

--- src/components/ImageReader.jsx
+++ src/components/ImageReader.jsx
@@ -1,7 +1,8 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 export default function ImageReader() {
     const [inputString, setInputString] = useState("");
+    const [outputString, setOutputString] = useState("")
     const [imageFile, setImageFile] = useState(null);
 
     const handleFile = (e) => {
@@ -9,10 +10,35 @@ export default function ImageReader() {
         setImageFile(URL.createObjectURL(e.target.files[0]));
     }
 
+    useEffect(() => {
+        let myHeaders = new Headers();
+        myHeaders.append("apikey", "K87327703988957");
+
+        let formdata = new FormData();
+        formdata.append("language", "eng");
+        formdata.append("url", { imageFile });
+        formdata.append("filetype", "png");
+        formdata.append("detectOrientation", "true");
+        formdata.append("scale", "true");
+
+        let requestOptions = {
+            method: 'POST',
+            headers: myHeaders,
+            body: formdata,
+            redirect: 'follow'
+          };
+
+        fetch("https://api.ocr.space/parse/image", requestOptions)
+        .then(response => response.text())
+        .then(result => setInputString(result))
+        .catch(error => console.log('error', error));
+    },[imageFile]);
+
   return (
     <div>
         <label htmlFor="image-upload">Select an image to upload</label>
         <input type="file" name="image-upload" onChange={ (e) => handleFile(e) } />
+        <textarea name="translated-text" value={ outputString } cols="30" rows="10"></textarea>
     </div>
   )
 }
