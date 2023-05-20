c4f746b27c0e9869b20c5750ad38e163a5612f9f
radiantbutterfly <alexandrialeemcpherson@gmail.com>
2023-05-20 21:26:00 +0900

    Implement base64 image conversion

--- src/components/ImageReader.jsx
+++ src/components/ImageReader.jsx
@@ -1,13 +1,28 @@
+import { Button } from "bootstrap";
 import { useEffect, useState } from "react"
 
 export default function ImageReader() {
     const [inputString, setInputString] = useState("");
     const [outputString, setOutputString] = useState("")
     const [imageFile, setImageFile] = useState(null);
+    const [base64file, setBase64File] = useState(null);
 
     const handleFile = (e) => {
         console.log(e.target.files)
-        setImageFile(URL.createObjectURL(e.target.files[0]));
+        setImageFile(e.target.files[0]);
+    }
+
+    const handleSubmit = () => {
+      if (imageFile) {
+        let reader = new FileReader();
+        reader.onload = () => {
+          setBase64File(reader.result);
+        }
+        reader.readAsDataURL(imageFile);
+      }
+      else {
+        alert("Please select a file.");
+      }
     }
 
     useEffect(() => {
@@ -15,9 +30,9 @@ export default function ImageReader() {
         myHeaders.append("apikey", "K87327703988957");
 
         let formdata = new FormData();
-        formdata.append("language", "eng");
-        formdata.append("url", { imageFile });
-        formdata.append("filetype", "png");
+        formdata.append("language", "jpn");
+        formdata.append("base64Image", { base64file });
+        formdata.append("filetype", "png")
         formdata.append("detectOrientation", "true");
         formdata.append("scale", "true");
 
@@ -32,13 +47,14 @@ export default function ImageReader() {
         .then(response => response.text())
         .then(result => setInputString(result))
         .catch(error => console.log('error', error));
-    },[imageFile]);
+    }, [base64file]);
 
   return (
     <div>
         <label htmlFor="image-upload">Select an image to upload</label>
         <input type="file" name="image-upload" onChange={ (e) => handleFile(e) } />
-        <textarea name="translated-text" value={ outputString } cols="30" rows="10"></textarea>
+        <button onClick={ handleSubmit }>Upload</button>
+        <textarea name="translated-text" readOnly={ true } value={ outputString } cols="30" rows="10"></textarea>
     </div>
   )
 }
