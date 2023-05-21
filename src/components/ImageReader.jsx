97918bdc9f32343c6d5df9352939a41df02201be
radiantbutterfly <alexandrialeemcpherson@gmail.com>
2023-05-21 10:05:41 +0900

    Change OCR to Tesseract

--- src/components/ImageReader.jsx
+++ src/components/ImageReader.jsx
@@ -1,5 +1,6 @@
 import { Button } from "bootstrap";
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
+import Tesseract from 'tesseract.js';
 
 export default function ImageReader() {
     const [inputString, setInputString] = useState("");
@@ -9,45 +10,48 @@ export default function ImageReader() {
 
     const handleFile = (e) => {
         console.log(e.target.files)
-        setImageFile(e.target.files[0]);
+        setImageFile(URL.createObjectURL(e.target.files[0]));
     }
 
     const handleSubmit = () => {
       if (imageFile) {
-        let reader = new FileReader();
-        reader.onload = () => {
-          setBase64File(reader.result);
-        }
-        reader.readAsDataURL(imageFile);
+        (async () => {
+          const worker = await Tesseract.createWorker();
+          await worker.loadLanguage('eng');
+          await worker.initialize('eng');
+          const { data: { text } } = await worker.recognize( { imageFile } );
+          console.log(text);
+          await worker.terminate();
+        })();
       }
       else {
         alert("Please select a file.");
       }
     }
 
-    useEffect(() => {
-        let myHeaders = new Headers();
-        myHeaders.append("apikey", "K87327703988957");
+    // useEffect(() => {
+    //     let myHeaders = new Headers();
+    //     myHeaders.append("apikey", "K87327703988957");
 
-        let formdata = new FormData();
-        formdata.append("language", "jpn");
-        formdata.append("base64Image", { base64file });
-        formdata.append("filetype", "png")
-        formdata.append("detectOrientation", "true");
-        formdata.append("scale", "true");
+    //     let formdata = new FormData();
+    //     formdata.append("language", "jpn");
+    //     formdata.append("base64Image", { base64file });
+    //     formdata.append("filetype", "png")
+    //     formdata.append("detectOrientation", "true");
+    //     formdata.append("scale", "true");
 
-        let requestOptions = {
-            method: 'POST',
-            headers: myHeaders,
-            body: formdata,
-            redirect: 'follow'
-          };
+    //     let requestOptions = {
+    //         method: 'POST',
+    //         headers: myHeaders,
+    //         body: formdata,
+    //         redirect: 'follow'
+    //       };
 
-        fetch("https://api.ocr.space/parse/image", requestOptions)
-        .then(response => response.text())
-        .then(result => setInputString(result))
-        .catch(error => console.log('error', error));
-    }, [base64file]);
+    //     fetch("https://api.ocr.space/parse/image", requestOptions)
+    //     .then(response => response.text())
+    //     .then(result => setInputString(result))
+    //     .catch(error => console.log('error', error));
+    // }, [base64file]);
 
   return (
     <div>
