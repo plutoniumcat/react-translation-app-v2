cf5053b1d7341c293ed7a83f23af9bde91c6bd50
JamJuiceCreative <jim@jamjuicecreative.com.au>
2023-05-23 20:28:55 +1000

    simple-nav rev1

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ export default function Navbar() {
     return <nav className="nav">
         <ul>
             <li>
-                <a href="/">Homepage</a>
+                <a href="/">Home</a>
             </li>
             <li>
                 <a href="/image">text to image</a>
