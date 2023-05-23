955108945b03dbdd2cc921b7bcd15d5a6d04df48
yops320 <liamdavidson4@gmail.com>
2023-05-23 21:31:14 +0930

    store user language selection

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,23 +1,45 @@
+import React, { useEffect } from "react";
+
+
 export default function Dropdown(props) {
-    const menuType = props.sourceLang ? "source-lang" : "output-lang";
-  
-    const languageSelect = (event) => {
-      const selectedLanguage = event.target.value;
-      if (props.setSourceLang) {
-        props.setSourceLang(selectedLanguage);
-        console.log(`Source language set to ${selectedLanguage}`);
-      } else if (props.setOutputLang) {
-        props.setOutputLang(selectedLanguage);
-        console.log(`Output language set to ${selectedLanguage}`);
-      } else {
-        console.log("Language dropdown could not detect if it is source or output.");
-      }
-    };
-  
-    return (
-      <div>
-        <label htmlFor={menuType}>Language:</label>
-        <select name={menuType} onChange={(e) => languageSelect(e)}>
+  const menuType = props.sourceLang ? "source-lang" : "output-lang";
+
+  const languageSelect = (event) => {
+    const selectedLanguage = event.target.value;
+    if (props.setSourceLang) {
+      props.setSourceLang(selectedLanguage);
+      console.log(`Source language set to ${selectedLanguage}`);
+      localStorage.setItem("sourceLang", selectedLanguage);
+    } else if (props.setOutputLang) {
+      props.setOutputLang(selectedLanguage);
+      console.log(`Output language set to ${selectedLanguage}`);
+      localStorage.setItem("outputLang", selectedLanguage);
+    } else {
+      console.log("Language dropdown could not detect if it is source or output.");
+    }
+  };
+
+  useEffect(() => {
+    const savedSourceLang = localStorage.getItem("sourceLang");
+    const savedOutputLang = localStorage.getItem("outputLang");
+
+    if (props.setSourceLang && savedSourceLang) {
+      props.setSourceLang(savedSourceLang);
+    }
+
+    if (props.setOutputLang && savedOutputLang) {
+      props.setOutputLang(savedOutputLang);
+    }
+  }, [props.setSourceLang, props.setOutputLang]);
+
+  return (
+    <div>
+      <label htmlFor={menuType}>Language:</label>
+      <select
+        name={menuType}
+        onChange={(e) => languageSelect(e)}
+        value={props.sourceLang || props.outputLang}
+      >
           <option value="english">English</option>
           <option value="arabic">Arabic</option>
           <option value="azerbaijani">Azerbaijani</option>
