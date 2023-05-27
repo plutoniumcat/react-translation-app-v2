811fe21dd0d3a54317ac069600447634a1d71bef
JamJuiceCreative <jim@jamjuicecreative.com.au>
2023-05-27 11:56:21 +1000

    wrapping bootstrap styling

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -6,6 +6,7 @@ import libreTranslateAPI from './libreTranslateAPI';
 import UploadText from "./UploadText";
 import { libreLangMap } from "../data/languageMaps";
 import PreLoader1 from "./PreLoader1";
+import { Col, Row } from 'react-bootstrap';
 
 
 const languageMap = libreLangMap;
@@ -52,19 +53,32 @@ export default function Homepage() {
   }, []);
 
   return (
-    <div>
-      <UploadText setInput={setInput} />
-      <form onSubmit={handleTranslate} className="d-flex flex-column align-items-center">
-        <Dropdown value={sourceLang} sourceLang={sourceLang} setSourceLang={setSourceLang} />
-        <Recordinput input={input} setInput={setInput} />
-        <button type="submit">Translate</button>
-        <Dropdown value={outputLang} outputLang={outputLang} setOutputLang={setOutputLang} />
-        {isLoading ? (
-          <PreLoader1 /> // Show the loader while translating/ 2 second timeout function in PreLoader.js
-        ) : (
-          <Recordoutput outputText={translation} />
-        )}
-      </form>
-    </div>
-  );
+  <div>
+    <UploadText setInput={setInput} />
+    <Row className="justify-content-between align-items-stretch">
+      <Col md={6} className="mb-3">
+        <form onSubmit={handleTranslate} className="d-flex flex-column align-items-center">
+          <Dropdown value={sourceLang} sourceLang={sourceLang} setSourceLang={setSourceLang} />
+          <Recordinput input={input} setInput={setInput} />
+          <button type="submit" className="btn btn-primary m-1" >Translate</button>
+        </form>
+      </Col>
+      <Col md={6} className="mb-3">
+        <div className="d-flex flex-column align-items-center">
+          {isLoading ? (
+            <PreLoader1 /> // Show the loader while translating/ 2-second timeout function in PreLoader.js
+          ) : (
+            <>
+              <Dropdown value={outputLang} outputLang={outputLang} setOutputLang={setOutputLang} />
+              <Recordoutput outputText={translation} />
+            </>
+          )}
+        </div>
+      </Col>
+    </Row>
+  </div>
+);
+
+  
+  
 }
--- src/components/Recordinput.jsx
+++ src/components/Recordinput.jsx
@@ -1,20 +1,19 @@
-// Recordinput:keeps the translator app updated in real-time as the user types or modifies the input string
-
 import React from "react";
 
 export default function Recordinput({ input, setInput }) {
-    const handleInputChange = (event) => {
-        setInput(event.target.value); // Update the parent's input state
-        console.log("Input text:", event.target.value); //Test input 
-    };
+  const handleInputChange = (event) => {
+    setInput(event.target.value);
+    console.log("Input text:", event.target.value);
+  };
 
-    return (
-        <textarea
-            name="source-text"
-            cols="30"
-            rows="10"
-            value={input}
-            onChange={handleInputChange}
-        ></textarea>
-    );
+  return (
+    <textarea
+      name="source-text"
+      cols="30"
+      rows="10"
+      value={input}
+      onChange={handleInputChange}
+      className="input-textarea"
+    ></textarea>
+  );
 }
